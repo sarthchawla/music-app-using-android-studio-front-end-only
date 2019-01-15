197578f79eadd36890ea534aedc02fec704dcb7f
sarthchawla <schawla.cse16@chitkarauniversity.edu.in>
2019-01-15 07:21:24 +0530

    done

---
+++ app/src/main/java/com/example/sarthak/musicapp/playing.java
@@ -0,0 +1,13 @@
+package com.example.sarthak.musicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class playing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing);
+    }
+}
